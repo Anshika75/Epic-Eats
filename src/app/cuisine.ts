9af81aa179dc14d553d36921161c559104b50c02
Demi Lune <anshikathakur596@gmail.com>
2024-10-10 16:29:13 +0530

    Create cuisine.ts

---
+++ src/app/cuisine.ts
@@ -0,0 +1,52 @@
+import { CuisineType } from "./cuisine.model";
+
+export const CuisineData: CuisineType[] = [
+    {
+        id: '1',
+        name: 'Indian'
+    },
+    {
+        id: '2',
+        name: 'Chinese'
+    },
+    {
+        id: '3',
+        name: 'Italian'
+    },
+    {
+        id: '4',
+        name: 'Mexican'
+    },
+    {
+        id: '5',
+        name: 'Thai'
+    },
+    {
+        id: '6',
+        name: 'Japanese'
+    },
+    {
+        id: '7',
+        name: 'American'
+    },
+    {
+        id: '8',
+        name: 'French'
+    },
+    {
+        id: '9',
+        name: 'Mediterranean'
+    },
+    {
+        id: '10',
+        name: 'Middle Eastern'
+    },
+    {
+        id: '11',
+        name: 'Spanish'
+    },
+    {
+        id: '12',
+        name: 'German'
+    },
+];
