aa1c8f1fd278006140bbc09a95072d0b92670c88
Demi Lune <anshikathakur596@gmail.com>
2024-10-13 19:37:59 +0530

    Update recipe.model.ts

--- src/app/recipe.model.ts
+++ src/app/recipe.model.ts
@@ -11,4 +11,8 @@ export interface RecipeType {
     cuisine: string;
     steps: string[];  // array of strings
     date: Date;
+    favourite: boolean;  // New field: to mark if the recipe is a favorite
+    serves: string;  // New field: to specify how many people the recipe serves
+    timeTaken: string;  // New field: to specify the total time taken
+    tags: string[];  // New field: an array of tags for additional categorization
 }
