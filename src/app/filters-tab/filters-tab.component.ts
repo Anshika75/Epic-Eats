54bb044beb1c4f7e88b4082dead5984e5bba60ac
Demi Lune <anshikathakur596@gmail.com>
2024-10-14 00:19:35 +0530

    filters tab initiated

---
+++ src/app/filters-tab/filters-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filters-tab',
+  standalone: true,
+  imports: [],
+  templateUrl: './filters-tab.component.html',
+  styleUrl: './filters-tab.component.css'
+})
+export class FiltersTabComponent {
+
+}
