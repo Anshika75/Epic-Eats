366935344086621166a317a93324f75013e51881
Demi Lune <anshikathakur596@gmail.com>
2024-10-13 23:44:07 +0530

    Create recipe.service.ts

---
+++ src/app/recipe.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { RecipeType } from './recipe.model';
+import { CuisineType } from './cuisine.model';
+import { RecipeData } from './recipe';  // Importing sample recipe data
+import { CuisineData } from './cuisine';  // Importing sample cuisine data
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+
+  constructor() { }
+
+  // Fetch all recipes
+  getRecipes(): Observable<RecipeType[]> {
+    return of(RecipeData);  // Using imported recipe data
+  }
+
+  // Fetch all cuisines
+  getCuisines(): Observable<CuisineType[]> {
+    return of(CuisineData);  // Using imported cuisine data
+  }
+
+  // Filter recipes by cuisine
+  getRecipesByCuisine(cuisine: string): Observable<RecipeType[]> {
+    return of(RecipeData.filter(recipe => recipe.cuisine === cuisine));
+  }
+
+  // Filter recipes by dietary preference
+  getRecipesByDietary(type: 'veg' | 'non-veg'): Observable<RecipeType[]> {
+    return of(RecipeData.filter(recipe => recipe.type === type));
+  }
+}
