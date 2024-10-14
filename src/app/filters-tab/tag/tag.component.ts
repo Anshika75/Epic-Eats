b8b221eefaae25cfad2e6018d3b3035ff6d92a5c
Demi Lune <anshikathakur596@gmail.com>
2024-10-14 09:20:53 +0530

    Update tag.component.ts

--- src/app/filters-tab/tag/tag.component.ts
+++ src/app/filters-tab/tag/tag.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tag',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './tag.component.css'
 })
 export class TagComponent {
-
+  @Input() tag!: string;
 }
